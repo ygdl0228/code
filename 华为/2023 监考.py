a12008a9646e70960dd01c65c8edad9497379801
ygd <65707601+ygdl0228@users.noreply.github.com>
2023-04-24 16:31:38 +0800

    华为

---
+++ 华为/2023 监考.py
@@ -0,0 +1,42 @@
+# @Time    : 2023/4/24 11:04
+# @Author  : ygd
+# @FileName: 2023 监考.py
+# @Software: PyCharm
+
+'''
+http://codefun2000.com/p/P1195
+差分数组
+'''
+
+import sys
+import os
+import collections
+
+n = int(input())
+hashmap = collections.defaultdict(int)
+for _ in range(n):
+    a, b = map(int, input().split())
+    hashmap[a] += 1
+    hashmap[b + 1] -= 1
+
+stack = []
+for key, value in hashmap.items():
+    stack.append([key, value])
+
+stack.sort()
+# print(stack)
+cnt = 0
+pre_time = stack[0][0]
+ans = 0
+for time, state in stack:
+    delta = time - pre_time
+    if cnt == 0:
+        ans += delta
+    elif cnt == 1:
+        ans += delta * 3
+    else:
+        ans += delta << 2
+    pre_time = time
+    cnt += state
+print(ans)
+
