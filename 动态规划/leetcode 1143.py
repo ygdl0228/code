588651899b8b30e4f2b89a48ad3528a989ab09a3
ygd <65707601+ygdl0228@users.noreply.github.com>
2023-05-06 16:33:05 +0800

    动态规划

---
+++ 动态规划/leetcode 1143.py
@@ -0,0 +1,29 @@
+# @Time    : 2023/5/6 16:22
+# @Author  : ygd
+# @FileName: leetcode 1143.py
+# @Software: PyCharm
+
+'''
+leetcode
+1143. 最长公共子序列
+mid
+https://leetcode.cn/problems/longest-common-subsequence/description/
+'''
+
+
+class Solution(object):
+    def longestCommonSubsequence(self, text1, text2):
+        """
+        :type text1: str
+        :type text2: str
+        :rtype: int
+        """
+        m, n = len(text1), len(text2)
+        dp = [[0] * (n + 1) for _ in range(m + 1)]
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                if text1[i - 1] == text2[j - 1]:
+                    dp[i][j] = dp[i - 1][j - 1] + 1
+                else:
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+        return dp[m][n]
