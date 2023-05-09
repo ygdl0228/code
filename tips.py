bc367fb4edd243dcc2b9fa6b793a7c1c3e1a0c24
ygd <65707601+ygdl0228@users.noreply.github.com>
2023-05-09 22:18:22 +0800

    tips

---
+++ tips.py
@@ -0,0 +1,46 @@
+# @Time    : 2023/5/9 22:09
+# @Author  : ygd
+# @FileName: tips.py
+# @Software: PyCharm
+
+'''
+strip
+lstrip
+rstrip
+'''
+
+tmp = "__test__test__"
+print(tmp.strip("_"))
+print(tmp.lstrip("_"))
+print(tmp.strip("_"))
+
+'''
+startswith()
+endswith()
+'''
+
+tmp = "test"
+print(tmp.startswith("t"))
+print(tmp.endswith("h"))
+
+'''
+lower()
+upper()
+'''
+
+'''
+isalpha()
+isdigit()
+isalnum() 仅包含字母和数字
+'''
+
+'''
+replace(old,new,count)
+'''
+
+'''
+index(sub,start,end) 从左往右
+rindex()
+'''
+
+
