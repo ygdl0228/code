37d4d61f01c3b1933e86a1c14269e6768e8f1cbf
ygd <65707601+ygdl0228@users.noreply.github.com>
2023-05-29 16:56:22 +0800

    前缀和

--- 前缀和/leetcode 930.py
+++ 前缀和/leetcode 930.py
@@ -19,11 +19,10 @@ class Solution(object):
         :type goal: int
         :rtype: int
         """
-
         nums = [0] + nums
         n = len(nums)
-        for i in range(n - 1):
-            nums[i + 1] += nums[i]
+        for i in range(1, n):
+            nums[i] += nums[i - 1]
         has = {0: 1}
         ans = 0
         for i in range(n - 1):
